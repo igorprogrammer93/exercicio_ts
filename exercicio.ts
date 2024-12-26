392352947b25675da86210ab04e0e6388a33d6b0
igor_programmer <99368475+igorprogrammer93@users.noreply.github.com>
2024-12-26 15:37:06 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Teste das funções
+console.log(multiplicar(2, 3)); // Saída: 6
+console.log(saudar("João"));    // Saída: Olá João
